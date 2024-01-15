12e05cc8ad382d6ff397f0557404e3b4db988432
jagdishpatil19 <patiljagdish471@gmail.com>
2024-01-16 00:51:57 +0530

    reactive form completed in using material

--- hotel_managmnet/src/app/shared/shared.module.ts
+++ hotel_managmnet/src/app/shared/shared.module.ts
@@ -4,8 +4,8 @@ import { MatButtonModule } from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
-
-
+import {MatSelectModule} from '@angular/material/select';
+import {MatRadioModule} from '@angular/material/radio';
 @NgModule({
   declarations: [],
   imports: [
@@ -13,13 +13,17 @@ import {MatFormFieldModule} from '@angular/material/form-field';
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
-    MatIconModule
+    MatIconModule,
+    MatSelectModule,
+    MatRadioModule
   ],
   exports:[
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
-    MatIconModule
+    MatIconModule,
+    MatSelectModule,
+    MatRadioModule
   ]
 })
 export class SharedModule { }
